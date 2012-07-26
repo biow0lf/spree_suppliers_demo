b8dee403dfaabbcee2bb4c9238c5bd3cae0ac788
Igor Zubkov <icesik@altlinux.org>
2012-07-26 17:25:20 +0300

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mystore::Application.config.secret_token = '4a312b491f34a0d3fe99276a6d293affe82e0949351073deea1e97ac725420dc9e54cab5c2432411586ec4f359c0bb3281a86884eb5775a3d75b747f13cefddf'
